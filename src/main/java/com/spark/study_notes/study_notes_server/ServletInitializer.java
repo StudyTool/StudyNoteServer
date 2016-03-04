6214b505d52d3d13c151896acd14ffb25412fd3d
YunZhang2014 <jslgdxzy@gmail.com>
2016-03-04 20:34:00 +1030

    IC

---
+++ src/main/java/com/spark/study_notes/study_notes_server/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spark.study_notes.study_notes_server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudyNotesServerApplication.class);
+	}
+
+}
---
+++ src/main/java/com/spark/study_notes/study_notes_server/StudyNotesServerApplication.java
@@ -0,0 +1,12 @@
+package com.spark.study_notes.study_notes_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNotesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyNotesServerApplication.class, args);
+	}
+}
